3684d018074cb1963f833c27c586bcd0379e208a
idoheinemann <idohaineman@gmail.com>
2020-02-09 18:59:26 +0200

    improved gblogger

--- utils/gblogger.py
+++ utils/gblogger.py
@@ -2,7 +2,12 @@ import os
 
 
 class GBLogger:
+    __names = set()
+
     def __init__(self, name, use_file=False, file_path=None):
+        if name in self.__names:
+            raise KeyError(f"duplicated logger name: {name}")
+        self.__names.add(name)
         self.name = name
         self.use_file = use_file
         if use_file:
@@ -14,30 +19,34 @@ class GBLogger:
         self.allow_info = True
         self.allow_warning = True
         self.allow_fatal = True
-    
-    def __log_to_file(self, msg):
+
+    def __log_to_file(self, msg, msg_type):
         if self.use_file:
-            self.file.write(msg + '\n')
-    
-    def __print(self, msg):
-        print(msg)
-    
-    def __full_proc(self, msg, should_print):
+            self.file.write(self.__prep_file(msg, msg_type))
+            self.file.flush()
+
+    def __print(self, msg, msg_type):
+        print(self.__prep_print(msg, msg_type))
+
+    def __full_proc(self, msg, msg_type, should_print):
         if should_print:
-            self.__print(msg)
-        self.__log_to_file(msg)
-        
-    def __prep(self, msg, msg_type):
-        return f'[{msg_type}] {msg}'
-    
+            self.__print(msg, msg_type)
+        self.__log_to_file(msg, msg_type)
+
+    def __prep_print(self, msg, msg_type):
+        return f'[{self.name}:{msg_type}] {msg}'
+
+    def __prep_file(self, msg, msg_type):
+        return f'[{msg_type}] {msg}\r\n'
+
     def debug(self, msg):
-        self.__full_proc(self.__prep(msg, 'DEBUG'), self.allow_debug)
-    
+        self.__full_proc(msg, 'DEBUG', self.allow_debug)
+
     def info(self, msg):
-        self.__full_proc(self.__prep(msg, 'INFO'), self.allow_info)
-        
+        self.__full_proc(msg, 'INFO', self.allow_info)
+
     def warning(self, msg):
-        self.__full_proc(self.__prep(msg, 'WARN'), self.allow_warning)
-        
+        self.__full_proc(msg, 'WARN', self.allow_warning)
+
     def fatal(self, msg):
-        self.__full_proc(self.__prep(msg, 'FATAL'), self.allow_fatal)
+        self.__full_proc(msg, 'FATAL', self.allow_fatal)
